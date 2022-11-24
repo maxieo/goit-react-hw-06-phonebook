1b7c1f080bfe0c61a7ad9af6905e08fe348e1fe7
maxieo <97198812+maxieo@users.noreply.github.com>
2022-11-24 19:21:37 +0200

    я не понимаю что я делаю

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['contacts']
+}
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+})
+
+const persContactReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {}
+  reducer: persContactReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [
+        FLUSH,
+        REHYDRATE,
+        PAUSE,
+        PERSIST,
+        PURGE,
+        REGISTER]
+    }
+  })
 })
+
+export const persistor = persistStore(store)
